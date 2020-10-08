6d2b2f463d1229abc62dc04d32e6378f84914ed5
KimDaehyung <72463904+Daehyung-Dim@users.noreply.github.com>
2020-10-08 18:55:18 +0900

    Add files via upload
    웹소켓 추가

---
+++ WebSocket_prj/src/com/biz/socket/WebSocketServlet.java
@@ -0,0 +1,54 @@
+package com.biz.socket;
+
+
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+@ServerEndpoint("/websocket")
+public class WebSocketServlet {
+
+	private static Set<Session> clients = Collections
+			.synchronizedSet(new HashSet<Session>());
+
+	@OnMessage
+	public void onMessage(String message, Session session) throws IOException {
+		System.out.println(message);
+		synchronized (clients) {
+			// Iterate over the connected sessions
+			// and broadcast the received message
+			for (Session client : clients) {
+				if (!client.equals(session)) {
+					client.getBasicRemote().sendText(message);
+				}
+			}
+		}
+	}
+
+	@OnOpen
+	public void onOpen(Session session) {
+		// Add session to the connected sessions set
+		System.out.println(session);
+		clients.add(session);
+	}
+
+	@OnClose
+	public void onClose(Session session) {
+		// Remove session from the connected sessions set
+		clients.remove(session);
+	}
+}
+
+
+/*@ServerEndpoint 부분이 클라이언트에서 접속할 서버 주소이다
+
+@OnMessage는 클라이언트로부터 메시지가 도착했을 경우 처리 방법이며
+
+@OnOpen과 @onClose는 클라이언트가 접속을 할 때와 접속이 끊어졌을떄의 처리이다
+*/
